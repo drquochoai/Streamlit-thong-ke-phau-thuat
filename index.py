9c44a53eeab320fb702598eb7d70fc636ee99e54
Tran Quoc Hoai <bsquochoainew@gmail.com>
2024-07-14 17:38:19 +0700

    chore: Update file uploader to allow uploading any file type

--- index.py
+++ index.py
@@ -3,24 +3,27 @@ import pandas as pd
 import altair as alt
 
 from urllib.error import URLError
-import urllib.request
-urllib.request.urlretrieve("https://docs.google.com/spreadsheets/d/e/2PACX-1vQNpA9xv7ci1tGPdF1I-HwPdPWNvyryr5YNQvXOwxKRIWdOg5zPy-2xvXjrRoChqeb6QmwQX-qO4-uO/pub?output=xlsx", "files.xlsx")
+# import urllib.request
+# # urllib.request.urlretrieve("https://docs.google.com/spreadsheets/d/e/2PACX-1vQNpA9xv7ci1tGPdF1I-HwPdPWNvyryr5YNQvXOwxKRIWdOg5zPy-2xvXjrRoChqeb6QmwQX-qO4-uO/pub?output=xlsx", "files.xlsx")
 
 # @st.cache_data
 def get_UN_data():
-    AWS_BUCKET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQNpA9xv7ci1tGPdF1I-HwPdPWNvyryr5YNQvXOwxKRIWdOg5zPy-2xvXjrRoChqeb6QmwQX-qO4-uO/pub?output=csv"
-    AWS_BUCKET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQNpA9xv7ci1tGPdF1I-HwPdPWNvyryr5YNQvXOwxKRIWdOg5zPy-2xvXjrRoChqeb6QmwQX-qO4-uO/pub?output=xlsx"
-    AWS_BUCKET_URL = ""
-    with urllib.request.urlopen(AWS_BUCKET_URL) as f:
-        html = f.read()
-        df = pd.read_excel(html, sheet_name=0, engine='openpyxl')
-
-    # uploaded_file = st.file_uploader("Upload a file")
-    # if uploaded_file is not None:
-    #     # df = pd.read_csv(AWS_BUCKET_URL)
-    #     df = pd.read_excel(uploaded_file, sheet_name=0, engine='openpyxl')
+    # Hàm load file online, khi published
+    
+    # AWS_BUCKET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQNpA9xv7ci1tGPdF1I-HwPdPWNvyryr5YNQvXOwxKRIWdOg5zPy-2xvXjrRoChqeb6QmwQX-qO4-uO/pub?output=csv"
+    # AWS_BUCKET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQNpA9xv7ci1tGPdF1I-HwPdPWNvyryr5YNQvXOwxKRIWdOg5zPy-2xvXjrRoChqeb6QmwQX-qO4-uO/pub?output=xlsx"
+    # AWS_BUCKET_URL = "https://github.com/drquochoai/Streamlit-thong-ke-phau-thuat/raw/main/Thang%206%20PTTT.xlsx"
+    # with urllib.request.urlopen(AWS_BUCKET_URL) as f:
+    #     html = f.read()
+    #     df = pd.read_excel(html, sheet_name=0, engine='openpyxl')
 
 
+    # Hàm load file từ local, khi debug
+    
+    uploaded_file = st.file_uploader("Upload a file")
+    if uploaded_file is not None:
+        # df = pd.read_csv(AWS_BUCKET_URL)
+        df = pd.read_excel(uploaded_file, sheet_name=0, engine='openpyxl')
         return df.set_index("HOTEN1")
 
 try:
@@ -33,23 +36,23 @@ try:
     else:
         data = df.loc[danhSachBacSi]
         # data /= 1000000.0
-        st.write("### Danh sách bệnh nhân đã phẫu thuật", data.sort_index())
+        st.write("### Danh sách bệnh nhân đã phẫu thuật", data)
 
-        data = data.T.reset_index()
-        data = pd.melt(data, id_vars=["index"]).rename(
-            columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
-        )
-        st.header(data.length)
-        chart = (
-            alt.Chart(data)
-            .mark_area(opacity=0.3)
-            .encode(
-                x="year:T",
-                y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
-                color="Region:N",
-            )
-        )
-        st.altair_chart(chart, use_container_width=True)
+        # data = data.T.reset_index()
+        # data = pd.melt(data, id_vars=["index"]).rename(
+        #     columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
+        # )
+        # # st.header(data.length)
+        # chart = (
+        #     alt.Chart(data)
+        #     .mark_area(opacity=0.3)
+        #     .encode(
+        #         x="year:T",
+        #         y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
+        #         color="Region:N",
+        #     )
+        # )
+        # st.altair_chart(chart, use_container_width=True)
 except URLError as e:
     st.error(
         """
