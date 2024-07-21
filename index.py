b7c4be475c26faf62b508f6399848a01df237e61
Tran Quoc Hoai <bsquochoainew@gmail.com>
2024-07-21 21:36:53 +0700

    chore: Update requirements.txt with plotly dependency

--- index.py
+++ index.py
@@ -5,6 +5,7 @@ import io
 from tabulate import tabulate
 import datetime
 import my_data_process_library as mylib
+import plotly.express as px
 
 # from pygwalker.api.streamlit import StreamlitRenderer
 
@@ -30,11 +31,62 @@ st.markdown(
 # if df['NGAY'] type is date, assign new date: with month to day and day to month, keep year
 # if df['NGAY'] type is string, usually "12/07/2024 9:00", assign new date that get first 2 charactor as day, charactor 4 and 5 is month, and 7 to 10 is year
 #  """
+# Giả sử bạn đã có DataFrame 'data_GOC_NTMLN' với các cột 'TENPTDM' và 'A2'
+
+
+def classify_procedure(procedure_description):
+    keywords = {
+        "PTNSLN": ["sinh thiết phổi", "soi màng phổi sinh thiết", "nội soi lồng ngực", "kén khí", "cắt kén", "sinh thiết màng phổi", "cắt phổi không", "nội soi cắt màng ngoài tim", "giao cảm ngực", "hạch giao cảm", "soi lồng ngực", "soi lồng ngực sinh thiết"],
+        "U trung thất": ["trung thất"],
+        "Tuyến giáp": ["tuyến giáp", "cắt tuyến giáp", "cắt tuyến giáp"],
+        "cắt thùy phổi": ["thùy phổi"],
+        "Suy tĩnh mạch": ["laser", "giãn tĩnh mạch", "Lazer", "Laser"],
+        "Catheter cảnh hầm": ["để lọc máu", "catheter tĩnh mạch", "chọc tĩnh mạch cảnh"],
+        "AVF": ["để chạy thận", "nối thông"],
+        "Bypass mạch máu": ["thiếu máu mạn tính", "tắc động mạch chi cấp tính", "nối tĩnh mạch lách", "động mạch đùi sâu", "tắc mạch máu các chi", "bắc cầu gần điều", "bắc cầu tĩnh mạch", "tắc động mạch chi bán", "khâu vết thương mạch máu"],
+        "Bypass hẹp-tắc-phình ĐMC": ["bắc cầu động mạch chủ ngực", "bắc cầu động mạch chủ với các động", "thay đoạn động mạch chủ bụng trên và dưới thận", "phồng động mạch chủ bụng"],
+        "Chích xơ TM": ["Gây xơ tĩnh"],
+        "DLMP": ["ẫn lưu khí", "phổi liên tục", "lưu màng phổi", "dẫn lưu tối thiểu khoang", "dẫn lư­u khí"],
+        "LN mổ mở": ["bóc vỏ màng", "bóc màng phổi", "u máu", "u bã đậu", "u lành phần mềm", "chỉ thép", "cơ hoành", "định lồng ngực", "viêm màng ngoài tim", "điều trị gãy xương", "rút nẹp vít", "tháo nửa bàn", "tháo khớp", "rạch áp xe", "sinh thiết hạch cổ", "hạch cổ bảo", "vét hạch cổ", "Sinh thiết hạch", "mở ngực", "khí quản", "u phần mềm", "gỡ dính", "rò phần mềm", "vết thương mạch máu", "vết thương phần mềm", "u mạch máu", "nhiễm trùng vết mổ", "khâu đơn giản vết thương", "lưu màng tim qua đường", "dẫn lưu dịch khoang màng", "cầm máu do chảy máu", "mở cạnh cổ dẫn", "dẫn lưu khoang màng tim", "Mở khoang và giải phóng mạch", "Dẫn lưu màng ngoài tim", "PT dẫn lưu áp xe phổi", "hạch lao to vùng nách"],
+        "DSA_Stent graft ĐMC": ["stent Graft", "stent động mạch chủ"],
+        "động mạch cảnh": ["động mạch cảnh"],
+        "TT_VAC": ["áp lực âm"],
+        "TT_NLN": ["chụp cắt lớp vi tính", "bỏ tổ chức hoại tử", "lọc tổ chức hoại tử", "Rút sonde dẫn", "Gây dính màng phổi", "Rút buồng tiêm", "băng vết mổ", "Chọc hút dịch", "Chọc dò dịch", "hút khí màng", "Chọc hút khí", "bóng đối xung động", "dẫn lưu áp xe tồn", "Chọc hút", "chỉ khâu da", "Chọc dịch màng ngoài", "Chọc tháo dịch màng", "thiết phần mềm bằng phương", "Chọc dò và dẫn lưu màng", "phân phúc mạc"],
+        "TIM_CABG": ["mạch vành"],
+        "TIM_TBS": ["điều trị hẹp đường ra", "thông liên nhĩ", "thông liên thất", "tim một tâm thất", "hẹp van động mạch phổi", "hồi lưu tĩnh mạch phổi", "Fallot", "kênh nhĩ thất", "Band động mạch phổi", "còn ống động mạch", "không có van động mạch phổi", "hẹp đường ra thất phải"],
+        "TIM_thay van hai lá": ["thay van hai lá"],
+        "TIM_sửa van hai lá": ["tạo hình van hai lá", "sửa van tim", "nội soi sửa van hai lá"],
+        "TIM_sửa van ba lá": ["thay van ba lá đơn thuần"],
+        "TIM_thay van ĐMC": ["van động mạch chủ"],
+        "TIM_u nhầy": ["u nhầy tim"],
+        "PTNSLN_hạch giao cảm": ["giao cảm ngực", "hạch giao cảm"],
+        "PTNSLN_nuss": ["Nuss", "lõm ngực", "lõm lồng ngực"],
+        "DSA_Can thiệp nội mạch": ["Chụp, nong", "Chụp và can thiệp", "số hóa xóa nền", "nong động mạch ngoại", "nong cầu nối động", "Hybrid điều trị bệnh mạch máu"]
+    }
+
+    for category, keyword_list in keywords.items():
+        for keyword in keyword_list:
+            if keyword.lower() in procedure_description.lower():
+                return category
+    return ""  # Nếu không khớp với bất kỳ từ khóa nào
 
 
 try:
+    global sheeturl
     sheeturl = st.text_input(
         "Nhập link file excel", "https://docs.google.com/spreadsheets/d/e/2PACX-1vQNpA9xv7ci1tGPdF1I-HwPdPWNvyryr5YNQvXOwxKRIWdOg5zPy-2xvXjrRoChqeb6QmwQX-qO4-uO/pub?output=xlsx")
+    col1, col2, col3 = st.columns(3)  # Equal width columns
+    button23 = col1.button("2023")
+    button22 = col2.button("2022")
+    button21 = col3.button("2021")
+
+    if button23:
+        sheeturl = "https://github.com/drquochoai/Streamlit-thong-ke-phau-thuat/raw/main/files/2023.xlsx"
+    if button22:
+        sheeturl = "https://github.com/drquochoai/Streamlit-thong-ke-phau-thuat/raw/main/files/2022.xlsx"
+    if button21:
+        sheeturl = "https://github.com/drquochoai/Streamlit-thong-ke-phau-thuat/raw/main/files/2021.xlsx"
+
     df = mylib.get_UN_data(sheeturl)
 
     # dsbacsikhoaNTMLN = ["1342 TS.BS Nguyễn Anh Dũng", "6489 ThS. BS Trần Thúc Khang",
@@ -44,9 +96,10 @@ try:
     #     bs for bs in dsbacsikhoaNTMLN if bs in df["HOTEN1"].unique().tolist()]
     # # df["HOTEN1"].unique().tolist()
     keywords = ["Nguyễn Anh Dũng", "Trần Thúc Khang",
-                        "Lê Thị Ngọc Hằng", "Nguyễn Hồng Vinh",
-                        "Trần Quốc Hoài", "Phạm Ngọc Minh Thủy", "Phan Vũ Hồng Hải", "Lê Chí Hiếu", "Nguyễn Minh Trí Viên", "Bùi Trọng Đạt"]
-    filtered_DanhSachBacSi = [name for name in df["HOTEN1"].unique().tolist() if any(keyword.lower() in name.lower() for keyword in keywords)] 
+                "Lê Thị Ngọc Hằng", "Nguyễn Hồng Vinh",
+                "Trần Quốc Hoài", "Phạm Ngọc Minh Thủy", "Phan Vũ Hồng Hải", "Lê Chí Hiếu", "Nguyễn Minh Trí Viên", "Bùi Trọng Đạt"]
+    filtered_DanhSachBacSi = [name for name in df["HOTEN1"].unique().tolist(
+    ) if any(keyword.lower() in name.lower() for keyword in keywords)]
     # print(filtered_names)
 
     danhSachBacSi = st.multiselect(
@@ -76,10 +129,14 @@ try:
         # PT theo danh mục
         # st.header("Tổng số PT/TT theo danh mục:")
         unique_TENPTDM = data_GOC_NTMLN['TENPTDM'].unique()
-        so_luong = data_GOC_NTMLN.groupby('TENPTDM').size().reindex(unique_TENPTDM, fill_value=0)  
-        newdf = pd.DataFrame({'Tên PT/TT': unique_TENPTDM, 'Số lượng': so_luong}).reset_index(drop=True)
+        so_luong = data_GOC_NTMLN.groupby(
+            'TENPTDM').size().reindex(unique_TENPTDM, fill_value=0)
+        newdf = pd.DataFrame(
+            {'Tên PT/TT': unique_TENPTDM, 'Số lượng': so_luong}).reset_index(drop=True)
         total = newdf['Số lượng'].sum()
-        new_row = pd.DataFrame({'Tên PT/TT': ['Tổng số'], 'Số lượng': [total]})
+        newdf['Phân loại'] = newdf['Tên PT/TT'].astype(str).apply(classify_procedure)
+        new_row = pd.DataFrame({'Tên PT/TT': ['Tổng số'], 'Số lượng': [total], 'Phân loại': ["-"]})
+
         newdf = pd.concat([newdf, new_row], ignore_index=True)
         # st.write(newdf)
         # Generate an HTML table using tabulate
@@ -99,8 +156,10 @@ try:
         # # # # PTTT theo bác sĩ
         st.header("Tổng số PT/TT theo bác sĩ:")
         unique_HOTEN1 = data_GOC_NTMLN['HOTEN1'].unique()
-        so_luong = data_GOC_NTMLN.groupby('HOTEN1').size().reindex(unique_HOTEN1, fill_value=0)  
-        newdf = pd.DataFrame({'PTV': unique_HOTEN1, 'Số lượng': so_luong}).reset_index(drop=True)
+        so_luong = data_GOC_NTMLN.groupby(
+            'HOTEN1').size().reindex(unique_HOTEN1, fill_value=0)
+        newdf = pd.DataFrame(
+            {'PTV': unique_HOTEN1, 'Số lượng': so_luong}).reset_index(drop=True)
 
         sorted_df = newdf.sort_values('Số lượng', ascending=False)
         # Generate an HTML table using tabulate
@@ -120,9 +179,21 @@ try:
         #     )
         # )
         # st.altair_chart(chart, use_container_width=True)
-        st.header("Bảng full dữ liệu:")
-        st.write("### Danh sách bệnh nhân đã phẫu thuật", data_GOC_NTMLN)
+        # st.header("Bảng full dữ liệu:")
+        # st.write("### Danh sách bệnh nhân đã phẫu thuật", data_GOC_NTMLN)
 
+        # PT theo tháng
+        data_GOC_NTMLN['Tháng'] = pd.to_datetime(
+            data_GOC_NTMLN['NGAY']).dt.month
+        data_GOC_NTMLN['Năm'] = pd.to_datetime(data_GOC_NTMLN['NGAY']).dt.year
+        monthly_count = data_GOC_NTMLN.groupby(
+            ['Năm', 'Tháng']).size().reset_index(name='Số lượng')
+        st.header("Tổng số PT/TT theo tháng:")
+        # st.dataframe(monthly_count)
+        # st.bar_chart(monthly_count, x='Tháng', y='Số lượng', color='Năm')
+        fig = px.bar(monthly_count, x='Tháng', y='Số lượng',
+                     color='Số lượng', text='Số lượng')
+        st.plotly_chart(fig)
 except URLError as e:
     st.error(
         """
