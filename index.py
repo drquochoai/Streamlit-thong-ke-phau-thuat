43c6583f3b08a59cb741c3622c3288b4d3299740
Tran Quoc Hoai <bsquochoainew@gmail.com>
2024-07-21 11:10:50 +0700

    Refactor HTML table styling in index.py

--- index.py
+++ index.py
@@ -33,71 +33,74 @@ st.markdown(
 
 
 try:
-    sheeturl = st.text_input("Nhập link file excel", "https://docs.google.com/spreadsheets/d/e/2PACX-1vQNpA9xv7ci1tGPdF1I-HwPdPWNvyryr5YNQvXOwxKRIWdOg5zPy-2xvXjrRoChqeb6QmwQX-qO4-uO/pub?output=xlsx")
+    sheeturl = st.text_input(
+        "Nhập link file excel", "https://docs.google.com/spreadsheets/d/e/2PACX-1vQNpA9xv7ci1tGPdF1I-HwPdPWNvyryr5YNQvXOwxKRIWdOg5zPy-2xvXjrRoChqeb6QmwQX-qO4-uO/pub?output=xlsx")
     df = mylib.get_UN_data(sheeturl)
-    
-    dsbacsikhoaNTMLN = ["1342 TS.BS Nguyễn Anh Dũng", "6489 ThS. BS Trần Thúc Khang",
-                        "2670 ThS.BS.CKI Lê Thị Ngọc Hằng", "3663 ThS.BS Nguyễn Hồng Vinh",
-                        "2638 BS.CKI Trần Quốc Hoài", "4091 BS.CKI Phạm Ngọc Minh Thủy", "4972 ThS.BS Phan Vũ Hồng Hải", "6176 BS.CKI Lê Chí Hiếu"]
-    dsbacsikhoaNTMLN_filter = [
-        bs for bs in dsbacsikhoaNTMLN if bs in df["HOTEN1"].unique().tolist()]
+
+    # dsbacsikhoaNTMLN = ["1342 TS.BS Nguyễn Anh Dũng", "6489 ThS. BS Trần Thúc Khang",
+    #                     "2670 ThS.BS.CKI Lê Thị Ngọc Hằng", "3663 ThS.BS Nguyễn Hồng Vinh",
+    #                     "2638 BS.CKI Trần Quốc Hoài", "4091 BS.CKI Phạm Ngọc Minh Thủy", "4972 ThS.BS Phan Vũ Hồng Hải", "6176 BS.CKI Lê Chí Hiếu"]
+    # dsbacsikhoaNTMLN_filter = [
+    #     bs for bs in dsbacsikhoaNTMLN if bs in df["HOTEN1"].unique().tolist()]
+    # # df["HOTEN1"].unique().tolist()
+    keywords = ["Nguyễn Anh Dũng", "Trần Thúc Khang",
+                        "Lê Thị Ngọc Hằng", "Nguyễn Hồng Vinh",
+                        "Trần Quốc Hoài", "Phạm Ngọc Minh Thủy", "Phan Vũ Hồng Hải", "Lê Chí Hiếu", "Nguyễn Minh Trí Viên", "Bùi Trọng Đạt"]
+    filtered_DanhSachBacSi = [name for name in df["HOTEN1"].unique().tolist() if any(keyword.lower() in name.lower() for keyword in keywords)] 
+    # print(filtered_names)
+
     danhSachBacSi = st.multiselect(
         "Chọn bác sĩ", df["HOTEN1"].unique().tolist(),
-        dsbacsikhoaNTMLN_filter
+        filtered_DanhSachBacSi
     )
     if not danhSachBacSi:
         st.error("Chọn ít nhất 1 bác sĩ.")
     else:
         data_GOC_NTMLN = df.loc[df["HOTEN1"].isin(danhSachBacSi)]
-        
+
         # data /= 1000000.0
-        
-        
-        dulieu_baoCao = data_GOC_NTMLN[['MABN', 'HOTEN', 'NAMSINH', 'NGAY', 'TENPT', 'HOTEN1']].rename(columns={'MABN': 'PID', 'HOTEN': 'Tên BN', 'HOTEN1': 'PTV', 'NAMSINH': "Năm sinh", 'NGAY': "Ngày PT", 'TENPT': "Tên phẫu thuật"})
+
+        dulieu_baoCao = data_GOC_NTMLN[['MABN', 'HOTEN', 'NAMSINH', 'NGAY', 'TENPT', 'HOTEN1']].rename(
+            columns={'MABN': 'PID', 'HOTEN': 'Tên BN', 'HOTEN1': 'PTV', 'NAMSINH': "Năm sinh", 'NGAY': "Ngày PT", 'TENPT': "Tên phẫu thuật"})
         dulieu_baoCao = dulieu_baoCao.sort_values("Ngày PT")
         # add column "STT" as the first column of dulieu_baoCao, start from 1,2,3... to end
         dulieu_baoCao.insert(0, "STT", range(1, len(dulieu_baoCao) + 1))
         # st.write(dulieu_baoCao)
-        
+
         st.header("Tổng số PT/TT: " + str(dulieu_baoCao.shape[0]))
         with st.expander("Danh sách bệnh nhân"):
-
             html_table = tabulate(dulieu_baoCao.sort_values("Ngày PT").to_dict("records"),
-                                tablefmt="html", headers="keys")
+                                  tablefmt="html", headers="keys")
             st.markdown(f'{html_table}', unsafe_allow_html=True)
-        
-        
+
         # PT theo danh mục
-        st.header("Tổng số PT/TT theo danh mục:")
+        # st.header("Tổng số PT/TT theo danh mục:")
         unique_TENPTDM = data_GOC_NTMLN['TENPTDM'].unique()
-        newdf = pd.DataFrame({'Tên PT/TT': unique_TENPTDM, 'Số lượng': data_GOC_NTMLN.groupby(
-            'TENPTDM').size()}).reset_index(drop=True)
+        so_luong = data_GOC_NTMLN.groupby('TENPTDM').size().reindex(unique_TENPTDM, fill_value=0)  
+        newdf = pd.DataFrame({'Tên PT/TT': unique_TENPTDM, 'Số lượng': so_luong}).reset_index(drop=True)
         total = newdf['Số lượng'].sum()
         new_row = pd.DataFrame({'Tên PT/TT': ['Tổng số'], 'Số lượng': [total]})
         newdf = pd.concat([newdf, new_row], ignore_index=True)
         # st.write(newdf)
         # Generate an HTML table using tabulate
-        html_table = tabulate(newdf.to_dict("records"),
-                              tablefmt="html", headers="keys")
-        
-        # Modify the HTML to include a specific ID and style
-        html_table = f"""
-        <table id="dmpt">
-        {html_table[6:]}
-        <style>
-        table#dmpt tr:last-child {{ font-weight: bold; }}  # Target specific table
-        </style>
-        </table>
-        """
-
-        st.markdown(f'{html_table}', unsafe_allow_html=True)
-
+        with st.expander("PT/TT theo danh mục:"):
+            html_table = tabulate(newdf.to_dict("records"),
+                                  tablefmt="html", headers="keys")
+            html_table = f"""
+            <table id="dmpt">
+            {html_table[7:]}
+            <style>
+            table#dmpt tr:last-child {{ font-weight: bold; }}
+            </style>
+            </table>
+            """
+            st.markdown(f'{html_table}', unsafe_allow_html=True)
 
         # # # # PTTT theo bác sĩ
         st.header("Tổng số PT/TT theo bác sĩ:")
         unique_HOTEN1 = data_GOC_NTMLN['HOTEN1'].unique()
-        newdf = pd.DataFrame({'PTV': unique_HOTEN1, 'Số lượng': data_GOC_NTMLN.groupby(
-            'HOTEN1').size()}).reset_index(drop=True)
+        so_luong = data_GOC_NTMLN.groupby('HOTEN1').size().reindex(unique_HOTEN1, fill_value=0)  
+        newdf = pd.DataFrame({'PTV': unique_HOTEN1, 'Số lượng': so_luong}).reset_index(drop=True)
 
         sorted_df = newdf.sort_values('Số lượng', ascending=False)
         # Generate an HTML table using tabulate
